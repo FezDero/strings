e074975eff14cc0a01d6d5dfac39a6e164aed87f
FezDero <fezdero@gmail.com>
2024-11-29 19:59:38 +0500

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,6 @@
+example = 'Альтруизм'
+print ('Первый символ:', example[0])
+print ('Последний символ:', example[-1])
+print ('Вторая половина:', example[4:])
+print ('Наоборот:', example[::-1])
+print ('Каждый 2-ой символ:', example[1::2])
